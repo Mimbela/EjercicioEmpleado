cba120696745d469b20552dae54f24c5f75e4da8
senin8 <malizziaaries@gmail.com>
2018-05-02 23:38:23 +0200

    Add project files.

---
+++ EjercicioEmpleado/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEmpleado
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public int Sueldo { get; set; }
+        public string Cargo { get; set; }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Me llamo {0} , mi sueldo es de {1} y mi cargo es {2}",
+                Nombre, Sueldo, Cargo);
+        }
+        //creamos el constructor para la parte fija
+        public Empleado()
+        {
+            Nombre = "Nuevo empleado";
+            Sueldo = 1000;
+            Cargo = "Empleado base";
+        }
+    }
+}
---
+++ EjercicioEmpleado/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEmpleado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado();
+            Console.WriteLine("Dime tu nombre");
+            string Nombre = Console.ReadLine();
+
+            if (Nombre == string.Empty)
+            {
+                empleado.MostrarDatos();
+            }
+            else
+            {
+                empleado.Nombre = Nombre;
+
+                Console.WriteLine("Dime tu sueldo");
+                empleado.Sueldo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dime tu cargo");
+                empleado.Cargo = Console.ReadLine();
+
+                empleado.MostrarDatos();
+            }
+            
+
+            Console.Read();
+
+        }
+    }
+}
+
+
+////(3.5). Crear un programa que contenga la clase ‘Empleado.cs’ con tres atributos “nombre”
+//,“sueldo” y “cargo” y un método
+//“imprimir”. 
+////El programa cliente solicitará al usuario su nombre y en caso de que el usuario haga
+//intro el programa construirá el objeto con unos datos fijos o constantes:
+//(nombre =“Nuevo Empleado”,sueldo = 1000,cargo=“Empleado Base”), si introduce el nombre 
+//    procederá a solicitar el resto de datos.
+////Al final el programa imprimirá “El nombre del empleado es XXXX y su sueldo es de 
+//XXXX” cuyo cargo es “XXXXX
